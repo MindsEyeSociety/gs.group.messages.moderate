a52c4fabba2e15d10ada5045bcf54e00fe85ca29
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-09 11:36:22 -0700

    Updating the license and copyright.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -11,21 +11,21 @@ setup(name='gs.group.messages.moderate',
     long_description=open("README.txt").read() + "\n" +
                       open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-    keywords='groupserver member request private secret',
+    keywords='groupserver member moderate moderation administration',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
     url='http://groupserver.org/',
-    license='other',
+    license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
     include_package_data=True,
@@ -37,13 +37,13 @@ setup(name='gs.group.messages.moderate',
         'zope.interface',
         'zope.schema',
         'Zope2',
+        'gs.content.layout',
         'gs.content.form',
         'gs.group.base',
         'gs.group.home',
         'gs.group.member.base',
         'gs.group.messages.base',
         'Products.GSGroup',
-        # -*- Extra requirements: -*-
     ],
     entry_points="""
     # -*- Entry points: -*-
